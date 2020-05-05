a223cdb4d67f3359a0a3617b765052bb6eec8416
ccanyanwu <eme42c@gmail.com>
2020-05-05 20:49:45 +0100

    Fifth commit. Worked on the choice behaviour

--- app.js
+++ app.js
@@ -19,52 +19,47 @@ const start = document.querySelector('#start'),
  //counter to hold number of correctly answered questions 
  let correctAnsweredQuestionCounter = 0;
 
-  //counter to hold  current score 
+  //counter to hold current score 
   let currentScoreCounter = 0;
 
-  let currentIndex,  
+  let currentIndex,
+      activeQuestion,
       sortQuestions;
+   
+  // all 3 choices 
+  let choice1,
+      choice2,
+      choice3;
       
 //array of question object 
 const questionArr = [
         {
             question : "Who won the Ballon d'Or in 2015?",
-            choices : [
-                {option:"Messi", correct: false},
-                {option:"Ronaldo", correct: true}, 
-                {option:"Neymar", correct: false}] 
-            
+            choices : ["Messi", "Ronaldo", "Neymar"], 
+            answer : 2
         },
         {
             question : "Can you name the former Germany International who went on to become a professional wrestler in WWE?",
-            choices : [
-                {option:"Ter Stegen", correct: false},
-                {option:"Tim Wiese", correct: true}, 
-                {option:"Oliver Khan", correct: false}] 
-            
+            choices : ["Ter Stegen", "Tim Wiese","Oliver Khan"],
+            answer : 2
+    
         },
         {
             question : "Who is the oldest manager in Premier League history?",
-            choices : [
-                {option:"Roy Hodgson", correct: true},
-                {option:"Alex Ferguson", correct: false}, 
-                {option:"Pellegrini", correct: false}] 
+            choices : ["Roy Hodgson", "Alex Ferguson", "Pellegrini"],
+            answer : 1
             
         },
         {
             question : "Which country won the Russia 2018 World Cup?",
-            choices : [
-                {option:"Croatia", correct: false},
-                {option:"Brazil", correct: false}, 
-                {option:"France", correct: true}] 
+            choices : ["Croatia", "Brazil", "France"],
+            answer : 3
             
         },
         {
             question : "“La Liga” is the name of which European country’s professional football association?",
-            choices : [
-                {option:"Italy", correct: false},
-                {option:"Spain", correct: true}, 
-                {option:"Germany", correct: false}] 
+            choices : ["Italy", "Spain", "Germany"], 
+            answer : 2
             
         }
     ]
@@ -81,7 +76,9 @@ const startQuiz = () =>{
     start.classList.add('hide');
     header.classList.add('hide');
     instruction.classList.add('hide');
-
+    currentScores.classList.remove('hide');
+    correctlyAnsweredQuestions.classList.remove('hide');
+ 
     //timer to delay loading of quiz page by milliseconds
     setTimeout(() => { 
         quizSection.classList.remove('hide');   
@@ -91,130 +88,111 @@ const startQuiz = () =>{
 
     //shuffles the questions
     sortQuestions = questionArr.sort( () => {
-        return Math.random() - .5;  
+        return Math.random() - 0.5;  
     });
     nextQuestion();
 } 
 
-//resets the quiz questions
+//resets the quiz questions and hides next button 
 const resetState = () => {
     next.classList.add('hide');
     if (optionsHolder.firstChild) {
         optionsHolder.innerHTML = "";  
     }
-}
+};
 
 //takes us to the next question
 const nextQuestion = () => {
     resetState();
-    showQuestions(sortQuestions[currentIndex]);   
+    activeQuestion = sortQuestions[currentIndex];
+    showQuestions(activeQuestion);   
 }
 
 //displays the questions on the click of next button
-const showQuestions = (questionArr  => {
-    question.innerText = questionArr.question; 
-    questionArr.choices.forEach(choice => {
+const showQuestions = (questionArr) => {
+    question.textContent = questionArr.question;
+    
+    questionArr.choices.forEach((choice, index) => {
+        index++;
         const button = document.createElement('button'); 
-        button.innerHTML = `<span>${choice.option}</span>`; 
+        button.innerHTML = `<span>${choice}</span>`; 
         button.classList.add('abc');
-        
-        if (choice.correct) {
-            button.dataset.correct = choice.correct;
-            
-        }
-        button.addEventListener('click', checkAnswer); 
+        button.classList.add(`abc-${index}`);
+        optionsHolder.classList.remove('disable');
         optionsHolder.appendChild(button);
+        if (questionArr.answer === index) {
+            button.dataset.correct = questionArr.answer;
+        }
+        choice1 = document.querySelector('.abc-1');
+        choice2 = document.querySelector('.abc-2');
+        choice3 = document.querySelector('.abc-3');
+        button.addEventListener('click', checkAnswer);     
     });
     
-});
+};
 
 // checks the answers and adds the result 
 const checkAnswer = (event) => {
-    selectedOption = event.target;
-    correct = selectedOption.dataset.correct;
+    const selectedOption = event.target;
+    const correct = selectedOption.dataset.correct;
     if (correct) {
-        
-        currentScoreCounter += 20; 
-        currentScores.textContent = `Your current score: ${currentScoreCounter}%`;
-        currentScores.classList.remove('hide');
-    
-
         correctAnsweredQuestionCounter ++;
+        currentScoreCounter += 20; 
         correctlyAnsweredQuestions.textContent = `Correctly answered questions: ${correctAnsweredQuestionCounter}/5`;
-        correctlyAnsweredQuestions.classList.remove('hide'); 
-
+        currentScores.textContent = `Your current score: ${currentScoreCounter}%`;
+     optionsHolder.classList.add('disable');
+        selectedOption.classList.add('right');
     } else{
+        correctAnsweredQuestionCounter +=0;
         currentScoreCounter += 0; 
         currentScores.textContent = `Your current score: ${currentScoreCounter}%`;
-        currentScores.classList.remove('hide');
+        //currentScores.classList.remove('hide');
+     
+        correctlyAnsweredQuestions.textContent = `Correctly answered questions: ${correctAnsweredQuestionCounter}/5`
+        optionsHolder.classList.add('disable');
+        selectedOption.classList.add('wrong');
         
+        if (activeQuestion.answer === 1) {
+            choice1.classList.add('right');
+        }else if(activeQuestion.answer === 2) {
+            choice2.classList.add('right');
+        }else if(activeQuestion.answer === 3){
+            choice3.classList.add('right');
+        } 
 
-        correctAnsweredQuestionCounter +=0;
-        correctlyAnsweredQuestions.textContent = `Correctly answered questions: ${correctAnsweredQuestionCounter}/5`;
-        correctlyAnsweredQuestions.classList.remove('hide');
     }
 
-
-    Array.from(optionsHolder.children).forEach(button => {
-        setStatus(button, button.dataset.correct); 
-    }); 
-
+    optionsHolder.setAttribute('disabled','disabled');
     
-
     if (sortQuestions.length > currentIndex + 1) {
         next.classList.remove('hide');
    
     } else {
         showResults(); 
     }
-
-
-
-}
-
-//this changes the color of the button
-const setStatus = (element, correct) => {
-    clearStatus(element); 
-    if (correct) { 
-        element.classList.add('right'); 
-        element.classList.remove('wrong');
-    }
-     else {
-        element.classList.add('wrong');  
-    }
-    element.setAttribute('disabled', 'disabled');
-}
-
-const clearStatus = (element) => {
-    element.classList.remove('right');
-    element.classList.remove('wrong'); 
 }
 
 //this shows the result
 const showResults = () => {
     main.innerHTML = '';
     document.body.classList.add('final');
-   // const finalMarkUp;
   currentScoreCounter;
-    /*const finalMarkUp = `<div class="final blinking">
-    <h2 class="final-score "> Your final score is ${currentScoreCounter}%</h2>
-    <input type="button" name="play" id="play" class="blinking" value="PLAY AGAIN" />
-  </div>`;*/
+  
   if(currentScoreCounter <= 20){
-     const finalMarkUp =  `<div class="final ">
+     const finalMarkUp = `<div class="final ">
     <h2 class="final-score blinking "> Your final score is ${currentScoreCounter}%</h2><p class="blink">you need whipping o!, anyway pay for your cable subscription and start watching from 1997</p>
     <input type="button" name="play" id="play" class="blinking" value="PLAY AGAIN" />
   </div>`
     main.insertAdjacentHTML('afterbegin', finalMarkUp);
   } else if(currentScoreCounter > 20 && currentScoreCounter <= 79){
-       const finalMarkUp =  `<div class="final ">
+       const finalMarkUp = `<div class="final ">
     <h2 class="final-score blinking"> Your final score is ${currentScoreCounter}%</h2><p class="blink">you watch just because you want to charge your phone in viewing center</p>
     <input type="button" name="play" id="play" class="blinking" value="PLAY AGAIN" />
   </div>`
     main.insertAdjacentHTML('afterbegin', finalMarkUp);
       
   }else {
-       finalMarkUp =  `<div class="final ">
+       finalMarkUp = `<div class="final ">
     <h2 class="final-score blinking"> Your final score is ${currentScoreCounter}%</h2><p class="blink" {
         constructor() {
             this.prop = ""
@@ -239,3 +217,4 @@ next.addEventListener('click', () => {
 
 //activates all event Listeners 
 eventListeners();
+
